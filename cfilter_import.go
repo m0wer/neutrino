eae1fa650415f6b928fa3f9aaebf8aaeb720eddb
m0wer <m0wer@sgn.space>
2026-08-23 21:27:54 +0200

    fix: migrate cfilter imports to btcd v2

--- cfilter_import.go
+++ cfilter_import.go
@@ -4,9 +4,9 @@ import (
 	"errors"
 	"fmt"
 
-	"github.com/btcsuite/btcd/btcutil/gcs"
-	"github.com/btcsuite/btcd/btcutil/gcs/builder"
-	"github.com/btcsuite/btcd/chaincfg/chainhash"
+	"github.com/btcsuite/btcd/btcutil/v2/gcs"
+	"github.com/btcsuite/btcd/btcutil/v2/gcs/builder"
+	"github.com/btcsuite/btcd/chainhash/v2"
 	"github.com/lightninglabs/neutrino/filterdb"
 )
 
